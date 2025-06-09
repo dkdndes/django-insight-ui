458bae40988979a846a67de8ca143b149fbd7ea8
Peter Rosemann <dkdndes@gmail.com>
2025-06-09 21:20:08 +0200

    aider: feat: Konfiguriere Template-Verzeichnis und füge i18n-Kontextprozessor hinzu

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "core", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -52,6 +52,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
             ],
         },
     },
