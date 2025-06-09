f039667f6d27234a3fd417469ae1dff8282ff060
Peter Rosemann <dkdndes@gmail.com>
2025-06-09 20:06:31 +0200

    aider: feat: Verbessere Sprachauswahl-Logik mit CSRF-Token-Handling

--- tests/urls.py
+++ tests/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("i18n/setlang/", "django.views.i18n.set_language", name="set_language"),
+    path("i18n/setlang/", set_language, name="set_language"),
 ]
 
 urlpatterns += i18n_patterns(
