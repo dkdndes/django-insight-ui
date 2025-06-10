a50457aedcd3fbb52c2690297c2150ef86a10a32
Peter Rosemann <dkdndes@gmail.com>
2025-06-10 10:54:56 +0200

    aider: refactor: CSRF-Schutz für HTMX-Formulare aktivieren

--- insight_ui/views.py
+++ insight_ui/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.template.loader import render_to_string
 from django.utils.translation import gettext as _
