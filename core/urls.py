7b15daa56151f48f40f42b5e2ce605d46c0a17cf
Peter Rosemann <dkdndes@gmail.com>
2025-06-14 18:10:03 +0200

    aider: fix: URLs in core und insight_ui korrekt einbinden und importieren

--- core/urls.py
+++ core/urls.py
@@ -2,17 +2,16 @@
 
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.i18n import set_language
-
-from insight_ui.views import insight_ui
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("i18n/setlang/", set_language, name="set_language"),
+    path("insight_ui/", include("insight_ui.urls")),
 ]
 
 urlpatterns += i18n_patterns(
-    path("", insight_ui, name="home"),
+    path("", include("insight_ui.urls")),
     prefix_default_language=False,
 )
--- insight_ui/urls.py
+++ insight_ui/urls.py
@@ -1,26 +1,20 @@
 from django.urls import path
-from views import (
-    component_demo_view,
-    htmx_form_submit,
-    live_data_view,
-    more_items_view,
-    normal_form_submit,
-    toggle_view,
-)
+
+from . import views
 
 urlpatterns = [
-    path("api/live-data/", live_data_view, name="live_data"),
-    path("api/more-items/", more_items_view, name="more_items"),
-    path("api/form-submit/", htmx_form_submit, name="htmx_form_submit"),
+    path("api/live-data/", views.live_data_view, name="live_data"),
+    path("api/more-items/", views.more_items_view, name="more_items"),
+    path("api/form-submit/", views.htmx_form_submit, name="htmx_form_submit"),
     path(
         "api/normal-form-submit/",
-        normal_form_submit,
+        views.normal_form_submit,
         name="normal_form_submit",
     ),
     path(
         "components/<str:component_name>/",
-        component_demo_view,
+        views.component_demo_view,
         name="component_demo",
     ),
-    path("toggle_view/", toggle_view, name="toggle_view"),
+    path("toggle_view/", views.toggle_view, name="toggle_view"),
 ]
