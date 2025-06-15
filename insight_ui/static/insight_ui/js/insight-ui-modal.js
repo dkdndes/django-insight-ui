e372f7fc1547ccc607378603cf62dd90590020d8
Peter Rosemann <dkdndes@gmail.com>
2025-06-15 12:18:59 +0200

    aider: fix: Modal-Dialoge öffnen und schließen korrekt implementieren

--- insight_ui/static/insight_ui/js/insight-ui-modal.js
+++ insight_ui/static/insight_ui/js/insight-ui-modal.js
@@ -1,1 +1,18 @@
-InsightUI.Modal = { init: function () { /* Modal logic here */ } };
+InsightUI.Modal = {
+  init: function () {
+    document.querySelectorAll('[data-insight-toggle="modal"]').forEach(button => {
+      button.addEventListener('click', function () {
+        const targetId = this.getAttribute('data-insight-target');
+        const modal = document.getElementById(targetId);
+        if (modal) {
+          modal.classList.remove('hidden');
+          modal.querySelector('[data-insight-dismiss="modal"]').addEventListener('click', function () {
+            modal.classList.add('hidden');
+          });
+          InsightUI.utils.trapFocus(modal);
+        }
+      });
+    });
+  }
+};
+document.addEventListener('DOMContentLoaded', InsightUI.Modal.init);
