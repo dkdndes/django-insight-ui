5226f9b62fdebb53af688fa9d90e1363b5a68d40
Matti Schwartz <matti.schwartz@alpininsight.com>
2025-07-16 17:45:24 +0200

    fix: sidebar issue

--- insight_ui/static/insight_ui/js/insight-ui-sidebar.js
+++ insight_ui/static/insight_ui/js/insight-ui-sidebar.js
@@ -1,35 +1,45 @@
 InsightUI.Sidebar = {
   init: function () {
-    const sidebar = document.querySelector('[data-insight-sidebar]');
-    if (!sidebar) return;
+    const sidebars = document.querySelectorAll('[data-insight-sidebar]');
+    if (!sidebars) return;
 
-    // Funktion zum Öffnen der Sidebar
-    function openSidebar() {
-      sidebar.style.transform = 'translateX(0)';
-    }
+    for (const sidebar of sidebars)
+    {
+      const side = sidebar.getAttribute("data-insight-sidebar");
 
-    // Funktion zum Schließen der Sidebar
-    function closeSidebar() {
-      sidebar.style.transform = 'translateX(100%)';
-    }
+      // Sidebar closes automatically when the mouse leaves the sidebar
+      let autoClose = true;
 
-    // Sidebar initial verstecken
-    closeSidebar();
-
-    // Öffnen, wenn Maus nahe an der rechten Seite ist
-    document.addEventListener('mousemove', (e) => {
-      const threshold = 50; // Pixel Abstand vom rechten Rand
-      if (window.innerWidth - e.clientX < threshold) {
-        openSidebar();
-      } else {
-        closeSidebar();
+      // Funktion zum Öffnen der Sidebar
+      function openSidebar() {
+        sidebar.style.transform = 'translateX(0)';
       }
-    });
 
-    // Optional: Schließen, wenn Maus die Sidebar verlässt
-    sidebar.addEventListener('mouseleave', () => {
+      // Funktion zum Schließen der Sidebar
+      function closeSidebar() {
+        if (side == "right") sidebar.style.transform = 'translateX(100%)';
+        else if (side == "left") sidebar.style.transform = 'translateX(-100%)';
+      }
+
+      // Sidebar initial verstecken
       closeSidebar();
-    });
+
+      // Öffnen, wenn Maus nahe an der Fenster Seite ist
+      document.addEventListener('mousemove', (e) => {
+        const threshold = 50; // Pixel Abstand vom Rand
+        if (side == "right" && window.innerWidth - e.clientX < threshold) {
+          openSidebar();
+        }
+        else if (side == "left" && e.clientX < threshold) {
+          openSidebar();
+        }
+      });
+
+      // Optional: Schließen, wenn Maus die Sidebar verlässt
+      sidebar.addEventListener('mouseleave', () => {
+        if (autoClose) closeSidebar();
+      });
+    }
   }
 };
 document.addEventListener('DOMContentLoaded', InsightUI.Sidebar.init);
